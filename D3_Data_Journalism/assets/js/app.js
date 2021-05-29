4ccb0fa0ccb876d4a038ddb59670722e45e519db
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-29 12:44:21 -0400

    initial app.js push (templating)

--- D3_Data_Journalism/assets/js/app.js
+++ D3_Data_Journalism/assets/js/app.js
@@ -20,3 +20,94 @@ var svg = d3.select(".chart")
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(newsData) {
+
+    // Step 1: Parse Data
+    newsData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare= +data.healthcare;
+      data.abbr= +data.abbr;  
+    });
+
+    // Step 2: Create scale functions
+    var xScale = d3.scaleLinear()
+      .domain([8.5, d3.max(newsData, d=> d.poverty*1.2)])
+      .range([0, width]);
+
+    var yScale = d3.scaleLinear()
+      .domain([4.5, d3.max(newsData, d=> d.healthcare*1.2)])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    var xAxis = d3.axisBottom(xScale).ticks(7);
+    var yAxis = d3.axisLeft(yScale).ticks(11);
+
+    // Step 4: Append Axes to the chart
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    chartGroup.append("g")
+      .call(yAxis);
+
+    // Step 5: Create circles with state abbreviation labels
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(newsData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.poverty))
+        .attr("cy", d => yScale(d.healthcare))
+        .attr("r", "10")
+        .attr("fill", "blue")
+        .attr("opacity", "0.5");
+    
+    
+    let stateTextGroup=chartGroup.selectAll(".stateText")
+        .data(newsData)
+        .enter()
+        .append("text")
+        .text(d=>d.abbr)
+        .attr("x", (d=>xScale(d)))
+        .attr("y", (d=>yLinearScale(d+5)))
+        .attr("class","stateText")
+        .attr("font-size", "10");
+    
+    // Step 6: Initialize tool tip
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`<strong>${d.abbr}<strong><hr>Percent in Poverty: ${d.poverty} Percent Lacking Healthcare: ${d.healthcare}`);
+    });
+
+    // Step 7: Create tooltip in the chart
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    circlesGroup.on("mouseover", function(d, i) {
+        toolTip.show(d, this);
+    })
+        .on("mouseout", function(d, i) {
+            toolTip.hide(d);
+        });
+    
+    // Step 9: Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("% in Poverty");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("% Lacking Healthcare");
+    
+    // Step 10: Catch error function
+    }).catch(function(error) {
+        console.log(error);
+      });
